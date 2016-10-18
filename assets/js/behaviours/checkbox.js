7ff81e0f7af3b5b3bbcac3dc58703fdafdf7924b
Christoffer Palm <christoffer.palm@oddhill.se>
2016-10-18 13:22:39 +0200

    Various design and styling changes

---
+++ assets/js/behaviours/checkbox.js
@@ -0,0 +1,8 @@
+import $ from 'jquery';
+
+// Run when DOM is changed
+Drupal.behaviors.oddminCheckbox = {
+  attach(context) {
+    //$('.ui.checkbox', context).checkbox();
+  }
+};
---
+++ assets/js/behaviours/dropdown.js
@@ -0,0 +1,7 @@
+import $ from 'jquery';
+
+Drupal.behaviors.oddminDropdown = {
+  attach(context) {
+    //$('.ui.dropdown', context).dropdown();
+  }
+};
